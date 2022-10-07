825b269ec0f092417d31bae5447e68df8f684870
Usuario553 <108374636+Usuario553@users.noreply.github.com>
2022-10-07 09:26:19 -0400

    Add files via upload

---
+++ assets/preload/data/reefer-madness/Momento Diálogos.lua
@@ -0,0 +1,228 @@
+--Momento Diálogos por Nickobelit
+--Yo What's Up I'ts me Mickey Mouse and Fucking Love Weed
+--This is certified hood classic
+--Smoke Weed Every Day
+--Cheese,*Sonido Espantaviejas xd*
+function onStepHit()--Agregando lo q debió estar...
+if curStep == 32 then--ENCERIO SI LO USAS DEJA CRÉDITOS PORQUE ENCERIO TOMÓ MUCHO HACERLO
+makeLuaText('risap',"* c rie o tose no se xdxdxdxd*",1280,0,620)
+        setTextFont('risap','MilkyNice.ttf')
+        setTextSize('risap', 40);
+        setTextAlignment('risap', 'center')
+        setObjectCamera("risap", 'other');
+        addLuaText('risap')
+        end
+if curStep == 64 then
+makeLuaText('yo',"Yo...",1280,0,620)
+        setTextFont('yo','MilkyNice.ttf')
+        setTextSize('yo', 40);
+        setTextAlignment('yo', 'center')
+        setObjectCamera("yo", 'other');
+        addLuaText('yo')
+        removeLuaText('risap')
+        end
+if curStep == 68 then
+makeLuaText('what',"Yo What's...",1280,0,620)
+        setTextFont('what','MilkyNice.ttf')
+        setTextSize('what', 40);
+        setTextAlignment('what', 'center')
+        setObjectCamera("what", 'other');
+        addLuaText('what')
+        removeLuaText('yo')
+        end
+if curStep == 72 then
+makeLuaText('what',"Yo What's Up...",1280,0,620)
+        setTextFont('what','MilkyNice.ttf')
+        setTextSize('what', 40);
+        setTextAlignment('what', 'center')
+        setObjectCamera("what", 'other');
+        addLuaText('what')
+        removeLuaText('yo')
+        end
+if curStep == 77 then
+makeLuaText('FnafReferencia',"Yo What's Up I'ts Me...",1280,0,620)
+        setTextFont('FnafReferencia','MilkyNice.ttf')
+        setTextSize('FnafReferencia', 40);
+        setTextAlignment('FnafReferencia', 'center')
+        setObjectCamera("FnafReferencia", 'other');
+        addLuaText('FnafReferencia')
+        removeLuaText('what')
+        end
+if curStep == 81 then
+makeLuaText('Mickey',"Yo What's Up I'ts Me Mickey...",1280,0,620)
+        setTextFont('Mickey','MilkyNice.ttf')
+        setTextSize('Mickey', 40);
+        setTextAlignment('Mickey', 'center')
+        setObjectCamera("Mickey", 'other');
+        addLuaText('Mickey')
+        removeLuaText('FnafReferencia')
+        end
+if curStep == 86 then
+makeLuaText('Mouse',"Yo What's Up I'ts Me Mickey Mouse",1280,0,620)
+        setTextFont('Mouse','MilkyNice.ttf')
+        setTextSize('Mouse', 40);
+        setTextAlignment('Mouse', 'center')
+        setObjectCamera("Mouse", 'other');
+        addLuaText('Mouse')
+        removeLuaText('Mickey')
+        end
+if curStep == 88 then
+makeLuaText('and',"And...",1280,0,620)
+        setTextFont('and','MilkyNice.ttf')
+        setTextSize('and', 40);
+        setTextAlignment('and', 'center')
+        setObjectCamera("and", 'other');
+        addLuaText('and')
+        removeLuaText('Mouse')
+        end
+if curStep == 91 then
+makeLuaText('fuck',"And F*cking...",1280,0,620)
+        setTextFont('fuck','MilkyNice.ttf')
+        setTextSize('fuck', 40);
+        setTextAlignment('fuck', 'center')
+        setObjectCamera("fuck", 'other');
+        addLuaText('fuck')
+        removeLuaText('and')
+        end
+if curStep == 95 then
+makeLuaText('love',"And F*cking Love...",1280,0,620)
+        setTextFont('love','MilkyNice.ttf')
+        setTextSize('love', 40);
+        setTextAlignment('love', 'center')
+        setObjectCamera("love", 'other');
+        addLuaText('love')
+        removeLuaText('fuck')
+        end
+if curStep == 98 then
+makeLuaText('hierba',"And F*cking Love W**d",1280,0,620)
+        setTextFont('hierba','MilkyNice.ttf')
+        setTextSize('hierba', 40);
+        setTextAlignment('hierba', 'center')
+        setObjectCamera("hierba", 'other');
+        addLuaText('hierba')
+        removeLuaText('love')
+        end
+if curStep == 106 then
+removeLuaText('hierba')
+end
+if curStep == 768 then
+makeLuaText('this',"This...",1280,0,620)
+        setTextFont('this','MilkyNice.ttf')
+        setTextSize('this', 40);
+        setTextAlignment('this', 'center')
+        setObjectCamera("this", 'other');
+        setTextColor('this','808080')
+        addLuaText('this')
+        end
+if curStep == 772 then
+makeLuaText('is',"This Is...",1280,0,620)
+        setTextFont('is','MilkyNice.ttf')
+        setTextSize('is', 40);
+        setTextAlignment('is', 'center')
+        setObjectCamera("is", 'other');
+        setTextColor('is','808080')
+        addLuaText('is')
+        removeLuaText('this')
+        end
+if curStep == 774 then
+makeLuaText('a',"This Is A...",1280,0,620)
+        setTextFont('a','MilkyNice.ttf')
+        setTextSize('a', 40);
+        setTextAlignment('a', 'center')
+        setObjectCamera("a", 'other');
+        setTextColor('a','808080')
+        addLuaText('a')
+        removeLuaText('is')
+        end
+if curStep == 778 then
+makeLuaText('Certified',"This Is A Certified...",1280,0,620)
+        setTextFont('Certified','MilkyNice.ttf')
+        setTextSize('Certified', 40);
+        setTextAlignment('Certified', 'center')
+        setObjectCamera("Certified", 'other');
+        setTextColor('Certified','808080')
+        addLuaText('Certified')
+        removeLuaText('a')
+        end
+if curStep == 789 then
+makeLuaText('Hood',"This Is A Certified Hood...",1280,0,620)
+        setTextFont('Hood','MilkyNice.ttf')
+        setTextSize('Hood', 40);
+        setTextAlignment('Hood', 'center')
+        setObjectCamera("Hood", 'other');
+        setTextColor('Hood','808080')
+        addLuaText('Hood')
+        removeLuaText('Certified')
+        end
+if curStep == 796 then
+makeLuaText('Classic',"This Is A Certified Hood Classic",1280,0,620)
+        setTextFont('Classic','MilkyNice.ttf')
+        setTextSize('Classic', 40);
+        setTextAlignment('Classic', 'center')
+        setObjectCamera("Classic", 'other');
+        setTextColor('Classic','808080')
+        addLuaText('Classic')
+        removeLuaText('Hood')
+        end
+if curStep == 811 then
+makeLuaText('smoke',"Smoke...",1280,0,620)
+        setTextFont('smoke','MilkyNice.ttf')
+        setTextSize('smoke', 40);
+        setTextAlignment('smoke', 'center')
+        setObjectCamera("smoke", 'other');
+        setTextColor('smoke','186A3B')
+        addLuaText('smoke')
+        removeLuaText('Classic')
+        end
+if curStep == 817 then
+makeLuaText('hierba2',"Smoke W**d...",1280,0,620)
+        setTextFont('hierba2','MilkyNice.ttf')
+        setTextSize('hierba2', 40);
+        setTextAlignment('hierba2', 'center')
+        setObjectCamera("hierba2", 'other');
+        setTextColor('hierba2','186A3B')
+        addLuaText('hierba2')
+        removeLuaText('smoke')
+        end
+if curStep == 822 then
+makeLuaText('Every',"Smoke W**d Every...",1280,0,620)
+        setTextFont('Every','MilkyNice.ttf')
+        setTextSize('Every', 40);
+        setTextAlignment('Every', 'center')
+        setObjectCamera("Every", 'other');
+        setTextColor('Every','186A3B')
+        addLuaText('Every')
+        removeLuaText('hierba2')
+        end
+if curStep == 826 then
+makeLuaText('Day',"Smoke W**d Every Day",1280,0,620)
+        setTextFont('Day','MilkyNice.ttf')
+        setTextSize('Day', 40);
+        setTextAlignment('Day', 'center')
+        setObjectCamera("Day", 'other');
+        setTextColor('Day','186A3B')
+        addLuaText('Day')
+        removeLuaText('Every')
+        end
+if curStep == 831 then
+removeLuaText('Day')
+end
+if curStep == 2224 then
+makeLuaText('que',"Cheese",1280,-245,540)
+        setTextFont('que','MilkyNice.ttf')
+        setTextSize('que', 40);
+        setTextAlignment('que', 'center')
+        setObjectCamera("que", 'other');
+        setTextColor('que','FFC300')
+        addLuaText('que')
+        end
+if curStep == 2230 then
+makeLuaText('so',"*Sonido Espantaviejas xd*",1280,290,540)
+        setTextFont('so','MilkyNice.ttf')
+        setTextSize('so', 40);
+        setTextAlignment('so', 'center')
+        setObjectCamera("so", 'other');
+        setTextColor('so','FF0000')
+        addLuaText('so')
+        end
+end
